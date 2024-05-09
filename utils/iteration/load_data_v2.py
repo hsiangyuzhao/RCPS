161c8e6ef3fde19763c50e3d319616d2858e8fa2
Xiangyu Zhao <53631393+hsiangyuzhao@users.noreply.github.com>
2024-05-09 11:49:39 +0800

    Update for real SSL

--- utils/iteration/load_data_v2.py
+++ utils/iteration/load_data_v2.py
@@ -8,10 +8,24 @@ simple_affine = np.eye(4, 4)
 
 
 class TrainValDataPipeline:
-    def __init__(self, image_root: str, mode: str = 'labeled', label_ratio=1.0, random_seed=None):
-        assert mode in ('labeled', 'unlabeled')
+    """
+    Data pipeline that specifies training and testing data in semi-supervised training, used to evaluate the effectiveness of 
+    SSL by changing the ratio of labeled data.
+
+    Note that when using `TrainValDataPipeline`, all of the images should have labels; if you are working on real semi-supervision 
+    scenarios (i.e., some of the data are unlabeled), you should use `RealSemiSupervisionPipeline` instead.
+    """
+    def __init__(self, image_root: str, label_ratio=1.0, random_seed: int = None):
+        """
+        Args:
+            image_root: the data root for training and validation data, should contain 4 folders:
+                train_images, train_labels, val_images, val_labels;
+            label_ratio: flaot, the ratio of labeled data in SSL, remaining training data is viewed as unlabeled,
+                and their segmentation labels remain untouched during training;
+            random_seed: random seed for spliting labeled and unlabeled training data.
+        """
+        assert label_ratio > 0 and label_ratio < 1, 'The raio of labeled data in semi-supervised training should be within 0 to 1.'
         self.image_root = image_root
-        self.mode = mode
         self.label_ratio = label_ratio
         self.seed = random_seed
 
@@ -34,8 +48,7 @@ class TrainValDataPipeline:
                 'image': os.path.join(self.image_root, '{}_images'.format(split_mode), filename),
                 'name': filename
             }
-            if self.mode != 'unlabeled':
-                subject['label'] = os.path.join(self.image_root, '{}_labels'.format(split_mode), filename),
+            subject['label'] = os.path.join(self.image_root, '{}_labels'.format(split_mode), filename),
             subjects.append(subject)
         return subjects
 
@@ -46,3 +59,55 @@ class TrainValDataPipeline:
                                      transform=unlabeled_transform if unlabeled_transform else train_transform)
         valset = dataset(data=self.val_subjects, transform=val_transform)
         return trainset, unlabeled_trainset, valset
+
+
+class RealSemiSupervisionPipeline:
+    """
+    Data pipeline instance that specifies training and testing data in semi-supervised training.
+
+    This class is different from `TrainValDataPipeline`, which is used for real semi-supervision scenarios.
+
+    Note:
+        1. In `TrainValDataPipeline`, we require that all of the training images are labeled, and by changing 
+           the `label_ratio` argument, we change the ratio of training data that are viewed as unlabeled data.
+        2. In `RealSemiSupervisionPipeline`, labeled data and unlabeled data should be stored separately, and 
+           SSL is utilized to enhance segmentation performance with given unlabeled data.
+    """
+    def __init__(self, labeled_image_root: str, unlabeled_image_root: str):
+        """
+        Args:
+            labeled_image_root: the data root for labeled images, should contain 4 folders:
+                train_images, train_labels, val_images, val_labels;
+            unlabeled_image_root: the data root for unlabeled images, should contain 1 folder:
+                train_images
+        """
+        self.labeled_root = labeled_image_root
+        self.unlabeled_root = unlabeled_image_root
+        self.prepare_train_val_subjects()
+
+    def prepare_train_val_subjects(self):
+        self.train_subjects = self.get_subjects(self.labeled_root, 'train', True)
+        self.val_subjects = self.get_subjects(self.labeled_root, 'val', True)
+        self.unlabeled_train_subjects = self.get_subjects(self.unlabeled_root, 'train', False)
+
+    def get_subjects(self, data_path: str, split_mode: str, is_labeled: bool = False):
+        subjects = []
+        image_list = os.listdir(os.path.join(data_path, '{}_images'.format(split_mode)))
+        for index, filename in enumerate(image_list):
+            subject = {
+                'image': os.path.join(data_path, '{}_images'.format(split_mode), filename),
+                'name': filename
+            }
+            if is_labeled:
+                subject['label'] = os.path.join(data_path, '{}_labels'.format(split_mode), filename),
+            subjects.append(subject)
+        return subjects
+
+    def get_dataset(self, train_transform, val_transform, cache_dataset=False, unlabeled_transform=None):
+        dataset = CacheDataset if cache_dataset else Dataset
+        trainset = dataset(data=self.train_subjects, transform=train_transform)
+        unlabeled_trainset = dataset(data=self.unlabeled_train_subjects,
+                                     transform=unlabeled_transform if unlabeled_transform else train_transform)
+        valset = dataset(data=self.val_subjects, transform=val_transform)
+        return trainset, unlabeled_trainset, valset
+    
